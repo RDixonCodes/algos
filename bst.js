1947ad2e64d723fd90e180f0ee8ff28721b48e28
RDixonCodes <rhdixon79@gmail.com>
2023-10-04 22:18:15 -0700

    DFS Pre Order/DFS Post Order/DFS In Order

--- bst.js
+++ bst.js
@@ -114,6 +114,66 @@ class BST {
         return data;
     }
 
+    //DFS(PreOrder) steps - recursively: Create a variabel to store the values of nodes visited. 
+    //Store the root of the BST in a variable called current. Write a helper fucntion which accepts a node.
+    //Push the value of the node to the variable that stores the values.
+    //If the node has a left property, call the helper function with the left property on the node.
+    // If the node has a right property, call the helper function with the right property on the node.
+    //Invoke the helper function with the current variable. Return the array of values.
+
+    DFSPreOrder() {
+        var visited = []
+        // can use a variable (current) if you don't want to start at the root
+        function traverse(node){
+            visited.push(node.value);
+            if(node.left) traverse(node.left);
+            if(node.right) traverse(node.right);
+        }
+        traverse(this.root);
+        return visited;
+    }
+
+    //DFS(PostOrder) steps - recursively:
+    //Create a variable to store the values of nodes visited. Store the root of the BST in a variable called current.
+    //Write a helper function which accepts a node.
+    //If the node has a right property, call the helper function with the right property on the node
+    //Push the value of the node to the variable that stores the values
+    //Invoke the helper function with the current variable
+    //Return the array of values
+
+    DFSPostOrder() {
+        var visited = []
+
+        function helper(node) {
+            if(node.left) helper(node.left);
+            if(node.right) helper(node.right);
+            visited.push(node.value);
+        }
+        helper(this.root);
+        return visited;
+    }
+
+    //DFS(In Order) steps - recursively:
+    //Create a variabel to store the values of nodes visited.
+    //Store the root of the BST in a variable called current.
+    //Write a helper function which accepts a node.
+    //If the node has a left property, call the helper function with the left property on the node.
+    //Push the value of the node to the variable that stores the values.
+    //If the node has right property, call the helper function with the right property on the node.
+    //Invoke the helper function with the current variable.
+
+    DFSInOrder() {
+        var data = [],
+            current = this.root;
+            function traverse(node){
+                //refactor to remove conditional
+                node.left && traverse(node.left);
+                data.push(node.value);
+                node.right && traverse(node.right);
+            }
+            traverse(current);
+            return data;
+    }
 };
 
 var tree = new BST();
@@ -122,10 +182,15 @@ tree.insert(7)
 tree.insert(13)
 tree.insert(5)
 tree.insert(17)
+tree.insert(2)
+tree.insert(11)
 
 console.log(tree)
 console.log(tree.contains(5))
 console.log(tree.BFS())
+console.log(tree.DFSPreOrder())
+console.log(tree.DFSPostOrder())
+console.log(tree.DFSInOrder())
 
 //refactored version of INSERT.
 
@@ -159,10 +224,5 @@ console.log(tree.BFS())
 //     }
 // }
 
-//BFS(steps - iteratively) Create a queue(this can be an array) and a variable to store the values of nodes visited.
-//Place the root node in the queue. Loop as long as there is anything in the queue. Dequeue a node from the queue and
-//push the value of the node into the variable that stores the nodes. If there is a left property on the node dequeued -
-//add it the queue. If there is a right property on the node dequeued - add it to the queue.
-//Return the variable that stores the values.
 
 
