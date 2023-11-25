d654c2e865a9ee3b6eb3b377734c8a08bec2cb38
RDixonCodes <rhdixon79@gmail.com>
2023-11-24 23:38:22 -0800

    dsa work

--- sandbox.js
+++ sandbox.js
@@ -548,4 +548,48 @@ function getIndexToIns(arr, num) {
 }
 
 
-console.log(getIndexToIns([40, 60], 50));
+// console.log(getIndexToIns([40, 60], 50));
+
+// Return true if the string in the first element of the array contains all of the letters 
+// of the string in the second element of the array.
+
+// For example, ["hello", "Hello"], should return true because all of the letters in the 
+// second string are present in the first, ignoring case.
+
+// The arguments ["hello", "hey"] should return false because the string hello does not contain a y.
+
+// Lastly, ["Alien", "line"], should return true because all of the letters in line are present in Alien.
+
+function mutation(arr) {
+    let test = arr[1].toLowerCase();
+    let target = arr[0].toLowerCase();
+
+    for(let i = 0; i < test.length; i++){
+        if(target.indexOf(test[i]) < 0) return false;
+    }
+    return true;
+  }
+  
+//   console.log(mutation(["Mary", "army"]));
+
+// Write a function that splits an array (first argument) into groups the length 
+// of size (second argument) and returns them as a two-dimensional array.
+
+function chunkArrayInGroups(arr, size) {
+    let temp = [];
+    const result = [];
+
+    for(let i = 0; i < arr.length; i++) {
+        if(i % size !== size -1) temp.push(arr[i]);
+        else {
+            temp.push(arr[i]);
+            result.push(temp);
+            temp = [];
+        }
+    }
+    if(temp.length !== 0) result.push(temp);
+    return result;
+  }
+  
+  console.log(chunkArrayInGroups(["a", "b", "c", "d"], 2));
+  console.log(chunkArrayInGroups([0,1,2,3,4,5,6,7], 5));
