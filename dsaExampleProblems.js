e109aefe8ad43599d8a2aa1592b365f8ab6a8b9b
RDixonCodes <rhdixon79@gmail.com>
2025-06-26 22:02:59 -0700

    dsa update

--- dsaExampleProblems.js
+++ dsaExampleProblems.js
@@ -47,3 +47,40 @@ function same2(arr1, arr2){
 
 console.log("Frequency Counter");
 console.log(same2([1,2,3,4], [9,1,4,16]));
+
+// ANAGRAMS: Given two string, write a function to determine if the second string is an anagram of the first. 
+// An ANAGRAM is a word, phrase, or name formed by rearranging the letters of another, such as cinema, formed from iceman.
+
+function same3(word1, word2){
+    if(word1.length != word2.length){
+        return false;
+    }
+    let newWord1 = word1.split('');
+    let newWord2 = word2.split('');
+    let letterCounter1 = {};
+    let letterCounter2 = {};
+
+    for(let val of newWord1){
+        letterCounter1[val] = (letterCounter1[val] || 0 ) + 1;
+        console.log(letterCounter1);
+    }
+
+    for(let val of newWord2){
+        letterCounter2[val] = (letterCounter2[val] || 0) + 1;
+        console.log(letterCounter2);
+    }
+
+    for(let key in letterCounter1){
+        if(!(key in letterCounter2)) {
+            return false;
+        }
+        if(letterCounter2[key] !== letterCounter1[key]){
+            return false;
+        }
+    }
+    return true;
+}
+
+console.log("Same3");
+console.log(same3("rat", "car"));
+console.log(same3("awesome", "someawe"));
