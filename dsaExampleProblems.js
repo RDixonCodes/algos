15f6e6f7336994ad57a97c6d5f33dccd05e56265
RDixonCodes <rhdixon79@gmail.com>
2025-09-04 14:47:08 -0700

    update

--- dsaExampleProblems.js
+++ dsaExampleProblems.js
@@ -112,8 +112,10 @@ function sumZero(arr){
  */
 
 function countUniqueValues(arr) {
+    if(arr.length === 0) return 0;
     var i = 0;
     for(var j = 1; j < arr.length; j++){
+        //count unique values in array
         if(arr[i]!== arr[j]) {
             i++;
             arr[i] = arr[j];
@@ -124,3 +126,41 @@ function countUniqueValues(arr) {
 
 console.log("\nUnique Values");
 console.log(countUniqueValues([1,1,2,3,3,4,5,6,6,7]));
+console.log(countUniqueValues([1,5,6,6,7]));
+
+/** Write a funciton called maxSubarraySum which accepts an array of integers and a number called n.
+ * The function should calculate the maximum sume of n consecutive elements in the array.
+ */
+
+function maxSubarraySum(arr){
+    if(num > arr.length) {
+        return null;
+    }
+    var max = -Infinity;
+    for(let i = 0; i < arr.length - num + 1; i++){
+        temp = 0;
+        for(let j = 0; j < num; j++){
+            temp += arr[i +j];
+        }
+        if(temp > max){
+            max = temp;
+        }
+    }
+    return max;
+}
+
+console.log("\nMaxSubarraySum");
+console.log(maxSubarraySum([1,2,5,2,9,0,3],3));
+
+//refactored version
+
+function maxSubarraySum2(arr,num){
+    let maxSum = 0;
+    let tempSum = 0;
+    if(arr.length < num) return null;
+    for(let i = 0; i < arr.length; i++){
+        tempSum = tempSum - arr[i - num] + arr[i];
+        maxSum = Math.max(maxSum, tempSum);
+    }
+    return maxSum;
+}
